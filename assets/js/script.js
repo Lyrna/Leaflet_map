eec8fc9312710b515efdda43128866fe77a0bbf4
Lyrna <clarisse.hurion@gmail.com>
2021-01-05 17:07:26 +0100

    map avancée avec les marqueurs identiques et les pop up qui sont ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,31 +12,52 @@ zoom: 13 // starting zoom
 //contrôle de la carte -zoom et rotation
 map.addControl(new mapboxgl.NavigationControl());
 
-// //ICONS
-// //style général
-// var LeafIcon = L.Icon.extend({
-//     options: {
-//         iconSize:     [40, 40],
-//         iconAnchor:   [22, 94],
-//         popupAnchor:  [-3, -76]
-//     }
+//icons
+// var myIcon = L.icon({
+//     iconUrl: '../img/map/books-media.png',
+//     iconSize: [40, 40],
+//     iconAnchor: [22, 94],
+//     popupAnchor: [-3, -76]
 // });
 
-// //liste des images qui serviront d'icônes
-// var cultura = new LeafIcon({iconUrl: '../img/map/books-media.png'}),
-//     grandbuffet = new LeafIcon({iconUrl: '../img/map/food.png'}),
-//     hopital = new LeafIcon({iconUrl: '../img/map/medical.png'});
-//     espaceliberte = new LeafIcon({iconUrl: '../img/map/swimming-pools.png'});
-//     halles = new LeafIcon({iconUrl: '../img/map/shopping.png'});
+// L.marker([3.031239, 43.179771], {icon: myIcon}).addTo(map);
 
-// L.icon = function (options) {
-//     return new L.Icon(options);
-// };
+// Set Cultura icon
+var cultura = new mapboxgl.Marker({
+src: "../img/map/books-media.png",
+draggable: false
+}).setLngLat([3.031239, 43.179771])
+.setPopup(new mapboxgl.Popup().setHTML("<img src='https://media.cultura.com/media/shop/45/cache/1/image/500x/9df78eab33525d08d6e5fb8d27136e95/n/a/narbonne_1.jpg' width='100%'/><strong>Cultura</strong><br/>Livres, jeux vidéos, musique, matériel de dessin, ce magasin propose une variété de produits liés au divertissement."))
+.addTo(map);
 
-// // j'indique la position de mes icônes et pop-up au clic
-// L.marker([3.031239, 43.179771], {icon: cultura}).addTo(map).bindPopup("Cultura");
-// L.marker([2.993, 43.173767], {icon: grandbuffet}).addTo(map).bindPopup("Les Grands Buffets");
-// L.marker([3.000136, 43.180508], {icon: hopital}).addTo(map).bindPopup("Hôpital de Narbonne");
-// L.marker([2.9941015, 43.173885], {icon: espaceliberte}).addTo(map).bindPopup("Espace liberté");
-// L.marker([3.005095, 43.181171], {icon: halles}).addTo(map).bindPopup("Les Halles");
+//Set Les grands buffets icon
+var grandbuffet = new mapboxgl.Marker({
+url: "../img/map/food.png",
+draggable: false
+}).setLngLat([2.993, 43.173767])
+.setPopup(new mapboxgl.Popup().setHTML("<img src='https://static.actu.fr/uploads/2020/06/narbonne-buffets.jpg' width='100%'><br/><strong>Les Grands Buffets</strong><br/>Mondialement reconnu, Les Grands Buffets propose une grande variété de produits, viandes, fromages, charcuterie, légumes, fruits, desserts en tout genre pour tous les goûts."))
+.addTo(map);
 
+//set l'hopital de narbonne icon
+var hopital = new mapboxgl.Marker({
+url: "../img/map/medical.png",
+draggable: false
+}).setLngLat([3.000136, 43.180508])
+.setPopup(new mapboxgl.Popup().setHTML("<img src='https://images.lindependant.fr/api/v1/images/view/5a967b948fe56f73526ba8f8/large/image.jpg' width='100%'><br/><strong>Hôpital de Narbonne</strong><br/>En période de crise sanitaire mondiale, il est toujours bon de savoir où se trouve un établissement de santé"))
+.addTo(map);
+
+//set espace liberté
+var espaceliberte = new mapboxgl.Marker({
+url: "../img/map/swimming-pools.png",
+draggable: false
+}).setLngLat([2.9941015, 43.173885])
+.setPopup(new mapboxgl.Popup().setHTML("<img src='https://images.lindependant.fr/api/v1/images/view/5a95a5568fe56f5185130e33/large/image.jpg' width='100%'><br/><strong>Espace liberté</strong><br/>Vous êtes à la recherche d'une piscine, d'une patinoire, d'un bowling ou d'un espace de restauration ? Vous avez trouvé le bon endroit pour passer de bons moments entre amis"))
+.addTo(map);
+
+//les halles icon
+var halles = new mapboxgl.Marker({
+url: "../img/map/shopping.png",
+draggable: false
+}).setLngLat([3.005095, 43.181171])
+.setPopup(new mapboxgl.Popup().setHTML("<img src='https://media-cdn.tripadvisor.com/media/photo-s/0f/2d/ad/fe/le-parvis-des-halles.jpg' width='100%'><br/><strong>Les Halles</strong><br/>Allez faire vos courses aux Halles et vous aurez droit à une variété de produits consommables, pizza à emporter ou viande fraîche seront proposés par vos commercants préférés"))
+.addTo(map);
