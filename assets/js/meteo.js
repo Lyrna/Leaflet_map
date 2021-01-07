ef74fe27b56fed2ec154c9659c96de4583f4bc45
Lyrna <clarisse.hurion@gmail.com>
2021-01-07 13:24:36 +0100

    temp et vent arrondis a l unité et responsive cartes ok pour mobiles

--- assets/js/meteo.js
+++ assets/js/meteo.js
@@ -3,9 +3,9 @@ fetch(url).then((response) =>
  response.json().then((data) => {
    console.log(data);
    document.querySelector ('#city').innerHTML ="<i class='fas fa-city'></i>" + data.name;
-   document.querySelector ('#temp').innerHTML ="<i class='fas fa-thermometer-three-quarters'></i>" +  data.main.temp + ' °C';
+   document.querySelector ('#temp').innerHTML ="<i class='fas fa-thermometer-three-quarters'></i>" +  Math.round(data.main.temp) + ' °C';
    document.querySelector ('#humidity').innerHTML = "<i class='fas fa-tint'></i>" + data.main.humidity + ' %';
-   document.querySelector ('#wind').innerHTML = "<i class='fas fa-wind'></i>" + data.wind.speed  + ' m/sec';
+   document.querySelector ('#wind').innerHTML = "<i class='fas fa-wind'></i>" + Math.round(data.wind.speed)  + ' m/sec';
    document.querySelector ('#descriptionMeteo').innerHTML = "<img src='http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png'>" + data.weather[0].description;
  })
 
